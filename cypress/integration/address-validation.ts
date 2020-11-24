07f498d759440fb0316239ab64fd9d23dbedf20c
tarrio <73276750+tarrio@users.noreply.github.com>
2020-11-24 11:10:33 +0100

    feat: Added address validation according to `currencyOut` (#9)

---
+++ cypress/integration/address-validation.ts
@@ -0,0 +1,63 @@
+describe('Address validation', () => {
+  const width = 600;
+  const height = 600;
+
+  beforeEach(() => {
+    cy.viewport(width, height);
+  });
+
+  it('fills currency and amount data', () => {
+    cy.visit('/');
+    cy.get('[data-testid="vertical.step-amounts.amounts.currency-from-select.target"]').click();
+    cy.get(
+      '[data-testid="vertical.step-amounts.amounts.currency-from-select.content.item-BTCB"]',
+    ).click();
+    cy.get('[data-testid="vertical.step-amounts.amounts.currency-to-select.target"]').click();
+    cy.get(
+      '[data-testid="vertical.step-amounts.amounts.currency-to-select.content.item-BTC"]',
+    ).click();
+    cy.get('[data-testid="vertical.step-amounts.amounts.amount-from"]').type('1');
+  });
+
+  it('validates addresses', () => {
+    cy.get('[data-testid="vertical.step-amounts.swap-btn"]').should('be.disabled');
+
+    cy.get('[data-testid="vertical.step-amounts.receiving-address.native-input"]').type(
+      '{selectall}invalidaddress',
+    );
+    cy.get('[data-testid="vertical.step-amounts.swap-btn"]').should('be.disabled');
+
+    cy.get('[data-testid="vertical.step-amounts.receiving-address.native-input"]').type(
+      '{selectall}tbnb1k3cp6jl7z757hewt30nfvr97uywlxeap69glam',
+    );
+    cy.get('[data-testid="vertical.step-amounts.swap-btn"]').should('be.disabled');
+
+    cy.get('[data-testid="vertical.step-amounts.receiving-address.native-input"]').type(
+      '{selectall}bnb1a4fqkg7pth85qnzvnapjf9zuhtxdn0yqjuuml9',
+    );
+    cy.get('[data-testid="vertical.step-amounts.swap-btn"]').should('be.disabled');
+
+    cy.get('[data-testid="vertical.step-amounts.receiving-address.native-input"]').type(
+      '{selectall}0x38a3234377f7d3c749c8d7d02c9962eaa6800541',
+    );
+    cy.get('[data-testid="vertical.step-amounts.swap-btn"]').should('be.disabled');
+
+    cy.get('[data-testid="vertical.step-amounts.receiving-address.native-input"]').type(
+      '{selectall}bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh',
+    );
+    cy.get('[data-testid="vertical.step-amounts.swap-btn"]').should('be.disabled');
+
+    cy.get('[data-testid="vertical.step-amounts.receiving-address.native-input"]').type(
+      '{selectall}mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt',
+    );
+    cy.get('[data-testid="vertical.step-amounts.swap-btn"]').should('not.be.disabled');
+  });
+
+  it('invalidates form if "currencyOut" changes', () => {
+    cy.get('[data-testid="vertical.step-amounts.amounts.currency-to-select.target"]').click();
+    cy.get(
+      '[data-testid="vertical.step-amounts.amounts.currency-to-select.content.item-BTCE"]',
+    ).click();
+    cy.get('[data-testid="vertical.step-amounts.swap-btn"]').should('be.disabled');
+  });
+});
