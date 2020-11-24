bd44c7d19774191ea049fc218d178255b63fd207
tarrio <73276750+tarrio@users.noreply.github.com>
2020-11-24 10:16:04 +0100

    fix: Paddings (#8)

--- src/components/Space/index.tsx
+++ src/components/Space/index.tsx
@@ -2,8 +2,10 @@ import { PulsarThemeType } from '@swingby-protocol/pulsar';
 import styled, { css } from 'styled-components';
 import { rem } from 'polished';
 
+import { StylingConstants } from '../../modules/styles';
+
 type Props = {
-  size: keyof PulsarThemeType['pulsar']['size'];
+  size: keyof PulsarThemeType['pulsar']['size'] | 'widgetBannerPadding' | 'widgetVerticalPadding';
   shape?: 'fit' | 'fill';
 };
 
@@ -11,8 +13,20 @@ const fill = css`
   flex-grow: 1;
 `;
 
+const bannerPadding = css`
+  width: ${rem(StylingConstants.widgetBannerPadding)};
+  height: ${rem(StylingConstants.widgetBannerPadding)};
+`;
+
+const verticalPadding = css`
+  width: ${rem(StylingConstants.widgetVerticalPadding)};
+  height: ${rem(StylingConstants.widgetVerticalPadding)};
+`;
+
 export const Space = styled.div<Props>`
   width: ${({ theme, size }) => rem(theme.pulsar.size[size])};
   height: ${({ theme, size }) => rem(theme.pulsar.size[size])};
+  ${({ size }) => size === 'widgetBannerPadding' && bannerPadding};
+  ${({ size }) => size === 'widgetVerticalPadding' && verticalPadding};
   ${({ shape }) => shape === 'fill' && fill};
 `;
