691d0fe4752acfffe443928241a85de67549e3dd
Rubén <ruben@swingby.network>
2020-11-26 11:19:53 +0100

    fix: Handle exceptions in `getNetworkForCoin`

--- src/modules/store/swap/validate.tsx
+++ src/modules/store/swap/validate.tsx
@@ -5,6 +5,7 @@ import { getNetworkForCoin, isAddressValid } from '@swingby-protocol/sdk';
 
 import { isCoinSupported } from '../../coins';
 import { useSdkContext } from '../../sdk-context';
+import { logger } from '../../logger';
 
 const areCurrenciesValid = (
   state: Pick<DefaultRootState['swap'], 'currencyIn' | 'currencyOut' | 'amountUser'>,
@@ -36,7 +37,12 @@ export const useIsAddressOutValid = () => {
   const currencyOut = useSelector((state) => state.swap.currencyOut);
   const context = useSdkContext();
   return useMemo(() => {
-    const network = getNetworkForCoin(currencyOut);
-    return { isAddressOutValid: isAddressValid({ context, address: addressOut, network }) };
+    try {
+      const network = getNetworkForCoin(currencyOut);
+      return { isAddressOutValid: isAddressValid({ context, address: addressOut, network }) };
+    } catch (e) {
+      logger.error(e);
+      return { isAddressOutValid: false };
+    }
   }, [context, addressOut, currencyOut]);
 };
