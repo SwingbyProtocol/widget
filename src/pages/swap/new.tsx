6a5975b44a4df9653fe0fca68b25217957fc2c9c
Rubén <ruben@swingby.network>
2020-11-27 10:22:27 +0100

    refactor: Moved pages

---
+++ src/pages/swap/new.tsx
@@ -0,0 +1,11 @@
+import { Swap } from '../../components/Swap';
+import { GlobalStyles } from '../../modules/styles';
+
+export default function SwapNew() {
+  return (
+    <>
+      <GlobalStyles />
+      <Swap />
+    </>
+  );
+}
