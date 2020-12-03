9da9b9e11d683d2156226ac6f6958a6931ed6cb2
Rubén <ruben@swingby.network>
2020-12-03 16:32:50 +0100

    fix: Warning in banner widget

--- src/components/ModeWarning/styled.tsx
+++ src/components/ModeWarning/styled.tsx
@@ -1,6 +1,7 @@
 import { Mode } from '@swingby-protocol/sdk';
 import { rem } from 'polished';
 import styled, { css } from 'styled-components';
+import { StylingConstants } from '../../modules/styles';
 
 const test = css`
   background: ${({ theme }) => theme.pulsar.color.secondary.normal};
@@ -19,12 +20,22 @@ export const ModeWarningContainer = styled.div<{ mode: Mode }>`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
-  height: ${({ theme }) => rem(theme.pulsar.size.street)};
+  margin: ${rem(1)} ${rem(StylingConstants.widgetBannerPadding)};
+  width: calc(100% - ${rem(StylingConstants.widgetBannerPadding * 2)});
+  height: ${({ theme }) => rem(theme.pulsar.size.closet)};
   font-size: ${({ theme }) => rem(theme.pulsar.size.drawer)};
+  border-radius: 2px;
 
   ${({ mode }) => mode === 'test' && test};
   ${({ mode }) => mode === 'production' && chaos};
+
+  @media ${StylingConstants.mediaLayout.widgetSmall} {
+    border-radius: 0;
+    margin: 0;
+    width: 100%;
+    height: ${({ theme }) => rem(theme.pulsar.size.street)};
+    font-size: ${({ theme }) => rem(theme.pulsar.size.drawer)};
+  }
 `;
 
 export const Link = styled.a`
