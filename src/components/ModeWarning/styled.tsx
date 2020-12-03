2365c5c7d856e5b59ec78951eceea5f3bc8e7cd9
tarrio <73276750+tarrio@users.noreply.github.com>
2020-12-03 16:48:32 +0100

    fix: Warning in banner widget (#17)

--- src/components/ModeWarning/styled.tsx
+++ src/components/ModeWarning/styled.tsx
@@ -2,6 +2,8 @@ import { Mode } from '@swingby-protocol/sdk';
 import { rem } from 'polished';
 import styled, { css } from 'styled-components';
 
+import { StylingConstants } from '../../modules/styles';
+
 const test = css`
   background: ${({ theme }) => theme.pulsar.color.secondary.normal};
   color: ${({ theme }) => theme.pulsar.color.secondary.text};
@@ -19,12 +21,22 @@ export const ModeWarningContainer = styled.div<{ mode: Mode }>`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
-  height: ${({ theme }) => rem(theme.pulsar.size.street)};
+  margin: ${rem(1)} ${rem(StylingConstants.widgetBannerPadding)};
+  width: calc(100% - ${rem(StylingConstants.widgetBannerPadding * 2)});
+  height: ${({ theme }) => rem(theme.pulsar.size.closet)};
   font-size: ${({ theme }) => rem(theme.pulsar.size.drawer)};
+  border-radius: 2px;
 
   ${({ mode }) => mode === 'test' && test};
   ${({ mode }) => mode === 'production' && chaos};
+
+  @media ${StylingConstants.mediaLayout.widgetSmall} {
+    border-radius: 0;
+    margin: 0;
+    width: 100%;
+    height: ${({ theme }) => rem(theme.pulsar.size.street)};
+    font-size: ${({ theme }) => rem(theme.pulsar.size.drawer)};
+  }
 `;
 
 export const Link = styled.a`
