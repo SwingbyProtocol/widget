95aa718a9ed508c6bf4b1ba54e2d6ce47b9eb6ab
Rubén <ruben@swingby.network>
2020-12-09 16:44:05 +0100

    refactor: Got rid of `WidgetVerticalContainer`

--- src/scenes/SwapDetails/Vertical/index.tsx
+++ src/scenes/SwapDetails/Vertical/index.tsx
@@ -4,12 +4,12 @@ import { useRouter } from 'next/router';
 import { useMemo } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 
+import { Space } from '../../../components/Space';
 import { VerticalWidgetView } from '../../../components/VerticalWidgetView';
-import { WidgetVerticalContainer } from '../../../components/WidgetVerticalContainer';
 import { useSdkContext } from '../../../modules/sdk-context';
 import { useSwapDetails } from '../../useSwapDetails';
 
-import { ExplorerLink, ExplorerLinkCaret, ProgressContainer } from './styled';
+import { ExplorerLink, ExplorerLinkCaret, ProgressContainer, ExplorerContainer } from './styled';
 import { Top } from './Top';
 
 export const Vertical = () => {
@@ -33,32 +33,35 @@ export const Vertical = () => {
   }
 
   return (
-    <WidgetVerticalContainer>
-      <VerticalWidgetView
-        onClickBack={() => push(`${context.mode === 'test' ? '/test' : ''}/swap/new`)}
-        top={<Top swap={swap} data-testid={buildTestId('')} />}
-        data-testid={buildTestId('')}
-      >
-        <ProgressContainer>
-          <SwapProgress
-            messages={SwapProgress.defaultMessages({ locale })}
-            status={swap.status}
-            currencyIn={swap.currencyIn}
-            currencyOut={swap.currencyOut}
-            data-testid={buildTestId('bottom.step-indicator')}
-          />
-        </ProgressContainer>
-      </VerticalWidgetView>
+    <VerticalWidgetView
+      onClickBack={() => push(`${context.mode === 'test' ? '/test' : ''}/swap/new`)}
+      top={<Top swap={swap} data-testid={buildTestId('')} />}
+      data-testid={buildTestId('')}
+    >
+      <ProgressContainer>
+        <SwapProgress
+          messages={SwapProgress.defaultMessages({ locale })}
+          status={swap.status}
+          currencyIn={swap.currencyIn}
+          currencyOut={swap.currencyOut}
+          data-testid={buildTestId('bottom.step-indicator')}
+        />
+      </ProgressContainer>
       {explorerLink && (
-        <ExplorerLink
-          href={explorerLink}
-          target="_blank"
-          data-testid={buildTestId('explorer-link')}
-        >
-          <FormattedMessage id="widget.explorer-link-long" />
-          <ExplorerLinkCaret />
-        </ExplorerLink>
+        <>
+          <Space size="box" shape="fill" />
+          <ExplorerContainer>
+            <ExplorerLink
+              href={explorerLink}
+              target="_blank"
+              data-testid={buildTestId('explorer-link')}
+            >
+              <FormattedMessage id="widget.explorer-link-long" />
+              <ExplorerLinkCaret />
+            </ExplorerLink>
+          </ExplorerContainer>
+        </>
       )}
-    </WidgetVerticalContainer>
+    </VerticalWidgetView>
   );
 };
