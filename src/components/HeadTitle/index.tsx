72eaf554e9902ac83caebd763aa2dd6ca4a80e56
Rubén <ruben@swingby.network>
2020-12-15 13:06:11 +0100

    fix: Bring back HeadTitle

--- src/components/HeadTitle/index.tsx
+++ src/components/HeadTitle/index.tsx
@@ -4,14 +4,14 @@ import { useIntl } from 'react-intl';
 import { useWidgetPathParams } from '../../modules/path-params';
 
 export const HeadTitle = () => {
-  // const { mode, resource, hash } = useWidgetPathParams();
-  // const { formatMessage } = useIntl();
-  // const page = hash ? 'details' : 'form';
+  const { mode, resource, hash } = useWidgetPathParams();
+  const { formatMessage } = useIntl();
+  const page = hash ? 'details' : 'form';
   return (
     <Head>
-      {/* <title>
+      <title>
         {formatMessage({ id: `widget.tab-title.${resource}.${mode}.${page}` }, { hash })}
-      </title> */}
+      </title>
     </Head>
   );
 };
