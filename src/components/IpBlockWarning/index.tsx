77c814ac9aa2d1e792442d73d8b299e6ed7dd175
Rubén <ruben@swingby.network>
2020-12-24 18:23:59 +0100

    asdad

--- src/components/IpBlockWarning/index.tsx
+++ src/components/IpBlockWarning/index.tsx
@@ -1,20 +1,26 @@
 import { FormattedMessage } from 'react-intl';
 
+import { useWidgetLayout } from '../../modules/layout';
 import { Space } from '../Space';
 
 import { Container, Content, Countries, Warning } from './styled';
 
 export const IpBlockWarning = () => {
+  const layout = useWidgetLayout();
   return (
     <Container>
       <Content>
         <Warning>
           <FormattedMessage id="widget.ip-block" />
         </Warning>
-        <Space size="street" />
-        <Countries>
-          <FormattedMessage id="widget.blocked-countries" />
-        </Countries>
+        {layout !== 'widget-banner' && (
+          <>
+            <Space size="street" />
+            <Countries>
+              <FormattedMessage id="widget.blocked-countries" />
+            </Countries>
+          </>
+        )}
       </Content>
     </Container>
   );
