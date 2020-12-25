c96ddfff19d18c72e5d10e0648f8493c2c131071
Shoe Kure <rswkure@gmail.com>
2020-12-26 01:44:40 +0800

    feat: Translated to Chinese (#36)

--- src/modules/i18n/index.tsx
+++ src/modules/i18n/index.tsx
@@ -1,3 +1,12 @@
 /* eslint-disable import/no-internal-modules */
 
+import zh from './files/zh.json';
+import zh_TW from './files/zh-TW.json';
+import en from './files/en.json';
 export { default as en } from './files/en.json';
+
+const languages = {
+  en,
+  zh,
+  'zh-TW': zh_TW,
+};
