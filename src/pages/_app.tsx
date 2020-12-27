f9ebb7462c8cc09f7a099057bbfabe97dfcf454e
Rubén <ruben@swingby.network>
2020-12-27 16:05:46 +0100

    feat: i18n

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,19 +14,26 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const store = useStore();
   const defaultLocale = router.defaultLocale ?? 'en';
-  const locale = router.locale ?? defaultLocale;
+  const locale = (() => {
+    const result = router.locale ?? defaultLocale;
+    if (Object.keys(languages).includes(result)) {
+      return result as keyof typeof languages;
+    }
+
+    return 'en';
+  })();
   const theme =
     router.query.theme === 'light' ? 'light' : router.query.theme === 'dark' ? 'dark' : undefined;
 
   return (
     <PulsarThemeProvider theme={theme}>
-      <IntlProvider messages={languages.en} locale={locale} defaultLocale={defaultLocale}>
+      <IntlProvider messages={languages[locale]} locale={locale} defaultLocale={defaultLocale}>
         <PulsarGlobalStyles />
         <ReduxProvider store={store}>
           <WidgetLayoutProvider>
             <Favicon />
             <Head>
-              <title>{languages.en['widget.tab-title.generic']}</title>
+              <title>{languages[locale]['widget.tab-title.generic']}</title>
             </Head>
             <Component {...pageProps} />
           </WidgetLayoutProvider>
