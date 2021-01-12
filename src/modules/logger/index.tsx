0b4d1d5e8ecd3d7cb59492447c564a4c3e442490
Rubén <ruben@swingby.network>
2021-01-12 11:08:10 +0100

    fix: Updated instructions to see SDK logs

--- src/modules/logger/index.tsx
+++ src/modules/logger/index.tsx
@@ -9,10 +9,10 @@ export const logger = pino({ level: logLevel });
     if (typeof window === 'undefined') return;
 
     const values = (localStorage.getItem('debug') || '').split(',').map((it) => it.trim());
-    if (values.includes('*') || values.includes('swingby')) return;
+    if (values.includes('*') || values.includes('skybridge-sdk')) return;
 
     logger.info(
-      `To see the internal logs of the Skybridge SDK, run the following code in your console.\n\n%clocalStorage.debug = 'swingby';`,
+      `To see the internal logs of the Skybridge SDK, run the following code in your console.\n\n%clocalStorage.debug = 'skybridge-sdk';`,
       'color: orange;',
     );
   } catch (e) {}
