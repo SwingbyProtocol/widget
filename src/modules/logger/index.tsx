1c6be0017aae3f2d790e8a616d8a1acb1cf50118
Rubén <ruben@swingby.network>
2021-01-12 10:47:18 +0100

    feat: Added log with instructions to enable SDK logs

--- src/modules/logger/index.tsx
+++ src/modules/logger/index.tsx
@@ -3,3 +3,15 @@ import pino from 'pino';
 import { logLevel } from '../env';
 
 export const logger = pino({ level: logLevel });
+
+(() => {
+  try {
+    const values = (localStorage.getItem('debug') || '').split(',').map((it) => it.trim());
+    if (values.includes('*') || values.includes('swingby')) return;
+
+    logger.info(
+      `To see the internal logs of the Skybridge SDK, run the following code in your console.\n\n%clocalStorage.debug = 'swingby';`,
+      'color: orange;',
+    );
+  } catch (e) {}
+})();
