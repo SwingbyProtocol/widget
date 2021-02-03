b37c957dc4530b9231927577602a2035d845a197
Rubén <ruben@swingby.network>
2021-02-03 11:22:26 +0100

    feat: Save affiliate for 30 days

--- src/modules/env/index.tsx
+++ src/modules/env/index.tsx
@@ -1,2 +1,7 @@
 export const logLevel =
   process.env.NEXT_PUBLIC_LOG_LEVEL || (process.env.NODE_ENV === 'production' ? 'info' : 'trace');
+
+export enum LocalStorage {
+  AffiliateCode = 'affiliateCode',
+  AffiliateCodeSavedAt = 'affiliateCodeSavedAt',
+}
