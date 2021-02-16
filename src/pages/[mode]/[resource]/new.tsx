d65f3b306220335d308bd9c8f312f02e0d62e1f2
Rubén <ruben@swingby.network>
2021-02-16 19:40:40 +0100

    fix: Abort if IP check does not respond within 3s

--- src/pages/[mode]/[resource]/new.tsx
+++ src/pages/[mode]/[resource]/new.tsx
@@ -111,10 +111,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ req }) => 
             const controller = new AbortController();
             const signal = controller.signal;
 
-            setTimeout(() => {
-              controller.abort();
-            }, 1000);
-
+            setTimeout(() => controller.abort(), 3000);
             const result = await fetch<{ shouldBlock: boolean }>(
               `https://ip-check.swingby.network/api/v1/ip/${ip}/check?secret=${server__ipCheckSecret}`,
               { signal },
