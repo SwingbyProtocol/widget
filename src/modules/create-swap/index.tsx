cd48504be5ea984893e5662f46fb6560391eb9c7
tarrio <73276750+tarrio@users.noreply.github.com>
2021-08-27 13:57:48 +0200

    Update index.tsx

--- src/modules/create-swap/index.tsx
+++ src/modules/create-swap/index.tsx
@@ -48,7 +48,6 @@ export const useCreate = ({ resource }: { resource: SkybridgeResource }) => {
             addressReceiving,
             affiliateCode,
           });
-          console.log('swap', swap);
 
           logger.debug('createSwap() has finished', swap);
 
