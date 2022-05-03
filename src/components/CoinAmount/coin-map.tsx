8c7c7f7864aa1485f30e037e29290071708d3926
Agustin Pane <agustin.pane@gmail.com>
2022-05-03 18:52:50 +0200

    Improve wording vertical list

--- src/components/CoinAmount/coin-map.tsx
+++ src/components/CoinAmount/coin-map.tsx
@@ -2,13 +2,20 @@ import { getChainFor, SkybridgeChain, SkybridgeCoin } from '@swingby-protocol/sd
 
 export type CoinMap = Map<SkybridgeChain | 'skypools', SkybridgeCoin[]>;
 
+const isSkypool = (networkName: string): boolean => /SKYPOOL$/.test(networkName);
+
 export const buildCoinMap = (coins: SkybridgeCoin[]): CoinMap => {
   const map = new Map<SkybridgeChain | 'skypools', SkybridgeCoin[]>();
 
+  coins.sort((coinA, coinB) => {
+    if (isSkypool(coinA)) return -1;
+    if (isSkypool(coinB)) return 1;
+    return 0;
+  });
+
   coins.forEach((it) => {
-    const isSkypools = /SKYPOOL$/.test(it);
     const chain = getChainFor({ coin: it });
-    const key = isSkypools ? 'skypools' : chain;
+    const key = isSkypool(it) ? 'skypools' : chain;
     const value = map.get(key) ?? [];
     map.set(key, [...value, it]);
   });
