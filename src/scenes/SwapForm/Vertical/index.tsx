dbfb0daaf06882bf0bbf03cba2243b0cbbf27328
creamwhip <dev3@swingby.network>
2023-04-19 13:40:55 +0800

    finish revert prev commit

--- src/scenes/SwapForm/Vertical/index.tsx
+++ src/scenes/SwapForm/Vertical/index.tsx
@@ -140,8 +140,8 @@ export const Vertical = ({ resource }: VerticalProps) => {
         </Button>
       )}
 
-      <StakeEarn as="a" target="_blank" href="https://skybridge.info/pool">
-        <FormattedMessage id="widget.stake-btc-earn-rewards" />
+      <StakeEarn as="span">
+        <FormattedMessage id="widget.warning" />
       </StakeEarn>
     </VerticalWidgetView>
   );
